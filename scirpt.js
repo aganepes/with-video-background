fb40f1206c673dcff1f2cbdb6cd29a738bc507c9
Aganapes Ahmedow <aaganapes@gmail.com>
2024-12-02 15:54:21 +0300

    change to title

---
+++ scirpt.js
@@ -0,0 +1,7 @@
+const showcase=document.querySelector('.showcase')
+const toggleMenu = document.querySelector('.toggle')
+
+toggleMenu.addEventListener('click',function() {
+    toggleMenu.classList.toggle('active')
+    showcase.classList.toggle('active')
+})
